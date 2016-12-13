dc6087255d412535217125e579344a38bf996bfa
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2016-12-13 22:20:16 +0100

    Make amount of tower bases dynamic

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,29 +3,23 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
-	public GameObject cube;
-	private int fieldWidth;
-	private int fieldHeight;
+	public GameObject towerBase;
+	public GameObject gameField;
 
-	// Use this for initialization
 	void Start () {
 		Debug.Log ("GameController: Game Started");
-		fieldWidth = 20;
-		fieldHeight = 10;
 		this.initGamePlatform ();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 	}
 
 	void initGamePlatform() {
-		// Create Cube
-		for (int x = 0; x < fieldWidth; x++) {
-			for (int z = 0; z < fieldHeight; z++) {
-				GameObject newCube = Instantiate (cube);
-				newCube.AddComponent<Rigidbody> ();
-				newCube.transform.position = new Vector3 (cube.transform.position.x+x, 0.1f, cube.transform.position.z-z);
+		for (int x = 0; x < gameField.transform.localScale.x; x++) {
+			for (int z = 0; z < gameField.transform.localScale.z; z++) {
+				GameObject newTowerBase = Instantiate (towerBase);
+				newTowerBase.AddComponent<Rigidbody> ();
+				newTowerBase.transform.position = new Vector3 (towerBase.transform.position.x+x, 0.1f, towerBase.transform.position.z-z);
 			}
 		}
 	}
