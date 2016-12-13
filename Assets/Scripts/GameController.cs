23a9ceea7038f4af6d634cd95da82cdf1ff2381b
Marc Illien <marc.illien@siroop.ch>
2016-12-13 21:10:02 +0100

    Add GameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("GameController: Game Started");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
