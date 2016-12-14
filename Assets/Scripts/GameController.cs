b4a244041e42b5fd889635eb0602791291606099
Marc Illien <marc.illien@siroop.ch>
2016-12-14 23:48:51 +0100

    Save created towers in an array

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,26 +3,29 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
-	public GameObject towerBase;
-	public GameObject gameField;
+	public	GameObject towerBase;
+	public 	GameObject gameField;
+	private GameObject[,] towerBases;
+	private int gameFieldWidth = 0;
+	private int gameFieldHeight = 0;
 
 	void Start () {
-		Debug.Log ("GameController: Game Started");
+		this.gameFieldWidth = (int)(gameField.transform.localScale.x);
+		this.gameFieldHeight = (int)(gameField.transform.localScale.z);
+		this.towerBases = new GameObject[this.gameFieldWidth, this.gameFieldHeight];
 		this.initGamePlatform ();
 	}
 
-	void Update () {
-	}
-
 	void initGamePlatform() {
-		for (int x = 0; x < gameField.transform.localScale.x; x++) {
-			for (int z = 0; z < gameField.transform.localScale.z; z++) {
+
+		for (int x = 0; x < this.gameFieldWidth; x++) {
+			for (int z = 0; z < this.gameFieldHeight; z++) {
 				GameObject newTowerBase = Instantiate (towerBase);
 				newTowerBase.AddComponent<Rigidbody> ();
 				newTowerBase.transform.position = new Vector3 (towerBase.transform.position.x+x, 0.1f, towerBase.transform.position.z-z);
-				// TODO:: Set transform to Parent. 
-				// Doesn't work: newTowerBase.transform.SetParent (towerBase.transform);
+				this.towerBases [x, z] = newTowerBase;
 			}
 		}
+
 	}
 }
