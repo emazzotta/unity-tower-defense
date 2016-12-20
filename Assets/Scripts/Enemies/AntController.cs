c44950452b21a5816a1b235ad5dfcf4294257ca5
Marc Illien <marc.illien@siroop.ch>
2016-12-20 22:26:52 +0100

    Add AntController

---
+++ Assets/Scripts/Enemies/AntController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AntController : MonoBehaviour {
+
+	private GameObject baseStart;
+
+	// Use this for initialization
+	void Start () {
+		this.FindInititalWaypoint ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FindInitialWaypoint() {
+		Debug.Log ("Ant Controller Start");
+	}
+}
