d11065c244b300adc918da36cfde931c2d247b31
Marc Illien <marc.illien@siroop.ch>
2016-12-20 21:35:27 +0100

    Mark waypoints

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,12 +9,16 @@ public class GameController : MonoBehaviour {
 	private GameObject[,] towerBases;
 	private int gameFieldWidth = 0;
 	private int gameFieldHeight = 0;
+	private Color wayColor;
 
 	void Start () {
+		this.wayColor = Color.Lerp(Color.red, Color.green, 0F);
+
 		this.gameFieldWidth = (int)(gameField.transform.localScale.x);
 		this.gameFieldHeight = (int)(gameField.transform.localScale.z);
 		this.towerBases = new GameObject[this.gameFieldWidth, this.gameFieldHeight];
 		this.initGamePlatform ();
+		this.drawWayPoint ();
 	}
 
 	void initGamePlatform() {
@@ -22,11 +26,22 @@ public class GameController : MonoBehaviour {
 		for (int x = 0; x < this.gameFieldWidth; x++) {
 			for (int z = 0; z < this.gameFieldHeight; z++) {
 				GameObject newTowerBase = Instantiate (towerBase);
-				newTowerBase.transform.position = new Vector3 (towerBase.transform.position.x+x, 0.1f, towerBase.transform.position.z-z);
+				newTowerBase.transform.position = new Vector3 (towerBase.transform.position.x + x, 0.1f, towerBase.transform.position.z - z);
 				this.towerBases [x, z] = newTowerBase;
 				newTowerBase.transform.SetParent (towerBaseWrap.transform);
 			}
 		}
 
+		// We don't need the initial prefab element anymore, it's replaced by a cloned tower.
+		Destroy (towerBase);
+			
 	}
+
+	void drawWayPoint() {
+		for (int x = 0; x < this.gameFieldWidth; x++) {
+			GameObject baseX = this.towerBases [x, 5];
+			baseX.GetComponent<Renderer> ().material.color = wayColor;
+		}
+	}
+
 }
