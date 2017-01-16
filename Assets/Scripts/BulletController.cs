b0e5404f0290214ee0b3c638cec593b675f9fe23
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2017-01-16 22:22:30 +0100

    Start work on bullet controller

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class BulletController : MonoBehaviour {
 
+	private float speed;
+	public Transfrom target;
+
 	// Use this for initialization
 	void Start () {
-	
+		speed = 5f;	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Vector3 direction = target.position - this.transform.localPosition;
+		float distanceCurrentFrame = speed * Time.deltaTime;
+
+		if(direction.magnitude <= distanceCurrentFrame) {
+			// We reached the node
+			doBulletHit();
+		} else {
+			this.transform.Translate(direction.normalized * distanceCurrentFrame, Space.World);
+			/* this.transform.rotation = Quaternion.Lerp(this.transform.rotation, tar) */
+		}
 	}
 }
