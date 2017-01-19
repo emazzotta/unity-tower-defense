f84789b641651399bcf9a81c9028ac86c2ea6dca
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2017-01-19 23:20:51 +0100

    Add proper enemy spawning

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -5,13 +5,20 @@ public class SpawnController : MonoBehaviour {
 
 	public GameObject enemiesFolder;
 	public GameObject originalEnemy;
-	public int amountOfEnemies;
+	public int enemiesToSpawn;
+    private int enemiesSpawned;
 	
 	void Start () {
-		for (int i = 0; i < amountOfEnemies; i++) {
+        enemiesSpawned = 0;
+        InvokeRepeating("SpawnEnemy", 0, 2f);
+	}
+
+    void SpawnEnemy() {
+       if(enemiesSpawned < enemiesToSpawn) { 
 			GameObject anEnemy = Instantiate (originalEnemy);
 			anEnemy.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y + 0.1f, this.transform.position.z);
 			anEnemy.transform.SetParent (enemiesFolder.transform);
+			enemiesSpawned += 1;
 		}
-	}
+    }
 }
