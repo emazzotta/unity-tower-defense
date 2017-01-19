24cfaae8aea15ea735388e4a3f2caa994fcb36d5
Marc Illien <marc.illien@siroop.ch>
2017-01-19 20:43:17 +0100

    Add utils

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils : MonoBehaviour {
+
+	public static Color getRandomColor() {
+		return new Color( Random.value, Random.value, Random.value, 1.0f );
+	}
+
+}
